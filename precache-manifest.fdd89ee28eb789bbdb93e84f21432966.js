a3af4fd1e7374c18979ad09f68eed82ae484c00d
pamelac08 <pamelac08@gmail.com>
2020-06-06 14:04:49 -0500

    Updates

---
+++ precache-manifest.fdd89ee28eb789bbdb93e84f21432966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d81104531fb6ebdd06a4b0cc1d02fcc",
+    "url": "/clicky-game-new/index.html"
+  },
+  {
+    "revision": "69399bc538ff987edd5a",
+    "url": "/clicky-game-new/static/css/main.0eaebe9b.chunk.css"
+  },
+  {
+    "revision": "738586b0fb3f0104ff7c",
+    "url": "/clicky-game-new/static/js/2.e52cc51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game-new/static/js/2.e52cc51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69399bc538ff987edd5a",
+    "url": "/clicky-game-new/static/js/main.264abf7b.chunk.js"
+  },
+  {
+    "revision": "1e9b61845cc40281a61b",
+    "url": "/clicky-game-new/static/js/runtime-main.69b96282.js"
+  }
+]);
